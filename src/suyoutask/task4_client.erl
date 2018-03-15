fcee720bd701a8bf2a2dbccc5742026e33432d3a
Eric <870402433@qq.com>
2018-03-15 20:26:41 +0800

    commit   test 12                  question: spawn/1

--- src/suyoutask/task4_client.erl
+++ src/suyoutask/task4_client.erl
@@ -13,28 +13,122 @@
 -compile(export_all).
 
 
-%%start()  ->.
 
-login(Pid, Name) ->
-	Pid ! {self(), {name,Name}},
-	case whereis(Name) of
-		undefined ->
-			io:format("this Name is accept  ~n"),
-			register(server,Pid);
-		_Pid ->
-			io:format("this name  is Exist ~n")
-	end,
-	receive
-	{Pid, Response} ->
-		Response
+start() ->
+	start("localhost",999).
+
+start(IP, Port) ->
+	register(client,spawn(fun() -> connectS(IP,Port) end)).
+
+connectS(IP, Port) when is_port(Port) ->
+	case gen_tcp:connect(IP, Port, [binary, {packet, 0}]) of
+		{ok, Socket} ->
+			talk(Socket);
+		{error,Why}->
+			outInfo(c2,Why),
+			exit(error)
 	end.
 
-logout() ->
-	server ! {self(), {logout,"client logout"}},
-	exit(normal) .
+talk(Socket) ->
+	receive
+		{tcp,Socket,Pack} ->
+			io:format(" ~p~n", [binary_to_term(Pack)]),
+			talk(Socket);
+		{tcp_closed,Socket} ->
+			gen_tcp:close(Socket),
+			outInfo(?MODULE," Socket die");
+	% gen_tcp:close(Socket)
+		{opt,Pack} ->
+			% outInfo(c2,?LINE),
+			gen_tcp:send(Socket, term_to_binary(Pack)),
+			talk(Socket)
+	end.
+
+%% 注册操作   登入   登出   一对一聊天   一对多聊天  查询在线玩家
+regis(Name,Pwd) ->
+	% if is_null(Name) -> %% 这里调用方法，无法确定方法所调用的返回值
+	case is_null(Name) of
+		true ->
+			client ! {opt ,{register,{Name,Pwd}}};
+		false ->
+			{false,name_is_null}
+	end.
+login(Name,Pwd) ->
+	client !{opt ,{login,{Name,Pwd}}}.
+out(Name) ->
+	client !{opt ,{out,{Name}}}.
+mms(Name,Msg) ->
+	client !{opt ,{mms,{Name,Msg}}}.
+mmall(Msg) ->
+	client !{opt ,{mmall,{Msg}}}.
+onli() ->
+	client !{opt ,{onli}}.
+
+%% 验证字符串是否为空
+is_null(X)
+	when is_list(X) ->
+	% outInfo(X,X),
+	Len=length(string:strip(X)),
+	if Len>=1 ->
+		true;
+		true -> false
+	end.
+
+%% 输出错误行
+outInfo(ClassName,Line) ->
+	io:format("__~p______line: ~p~n",[ClassName,Line]).
+
+
+%%start(Name) ->
+%%	register(client, spawn(fun() -> login(Name) end)),
+%%	register(client1, spawn(fun() -> login1(Name) end)).
+%%
+%%login(Name) ->
+%%	server ! {self(), {name,Name}},
+%%	io:format("client send name ok~n"),
+%%%%	server ! {self(), Q},
+%%	receive
+%%		{server, Reply} ->
+%%			io:format("reply: ~p~n",[Reply])
+%%	end.
+%%
+%%
+%%login1(Name) ->
+%%	server ! {self(), {name,Name}},
+%%	io:format("client send name ok~n"),
+%%%%	server ! {self(), Q},
+%%	receive
+%%		{server, Reply} ->
+%%			io:format("reply: ~p~n",[Reply])
+%%	end.
+%%
+%%
+%%chat(Message) ->
+%%%%	Message=io:read(Other),
+%%	Other!{self(),{message,Other,Message}},
+%%	chat(Other) .
 
 
 
+
+
+%%logout(Name) ->
+%%	io:format("server pid: ~p",[server]),
+%%	server ! {self(), {logout, "client logout"}},
+%%	unregister(Name),
+%%	exit(normal) .
+%%%%	.
+%%
+%%chat(Other) ->
+%%	Message=io:read(Other),
+%%	Other!{self(),{message,Other,Message}},
+%%	chat(Other) .
+%%
+%%message_send(Pid) ->
+%%	Pid !{self(),{message,"message send ok"}}.
+%%
+%%
+%%
 
 
 
--- src/suyoutask/task4_server.erl
+++ src/suyoutask/task4_server.erl
@@ -15,33 +15,318 @@
 %% API
 -compile(export_all).
 
+-record(user, {name,socket,online,pwd,text}).
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, true}, {reuseaddr, true}]).
+-define (tabInfo, tabinfo).
+-define (clInfo, clinfoSpawn).
+-define (seInfo, serverInfo).
 
-start()   ->spawn(task4_server,login_loop,[]).
+str() ->
+	str(999).
 
-login_loop() ->
+str(Port) ->
+	register(?seInfo,spawn_link(fun() -> connect(Port) end)),
+	register(?clInfo,spawn_link(fun() -> tabLoop() end)).
+
+%% 端口监听 连接
+connect(Port) ->
+	{ok,LSocket} = gen_tcp:listen(Port,?TCP_OPTIONS),
+	accept_s(LSocket).
+
+%% 并行服务器，使每个客户端都有自己的socket，防止进程被占用无法操作请求
+accept_s(LSocket) ->
+	process_flag(trap_exit,true),
+	case gen_tcp:accept(LSocket) of
+		{ok,Socket} ->
+			spawn_link(fun() -> accept_s(LSocket) end),
+			loop(Socket);
+		{error,_Why} ->
+			exit(accept_s)
+	end.
+
+loop(Socket) ->
 	receive
-		{From, {name, Name}} ->
-
-			case whereis(Name) of
-			undefined ->
-				From!{self(),"this Name is accept  ~n"} ,
-				register(Name,From);
-				_Pid ->
-					From ! {self()," this name  is Exist ~n"}
+		{tcp,Socket,Bin} ->
+			Opt =binary_to_term(Bin),
+			?clInfo ! {opt,Socket,Opt},
+			loop(Socket);
+		{tcp_closed,Socket} ->
+			case select('$0',Socket,true) of
+				[] ->
+					noop ;
+				Obj ->
+					Prop = {out,{sel_f(Obj)}},
+					?clInfo ! {opt,Socket,Prop}
+				% outInfo(Socket,?LINE),
+				% out(Socket,sel_f(Obj))    %% 设置ets为public时才能操作
 			end,
-			login_loop()
+			outInfo(?MODULE,tcp_closed),
+			loop(Socket)
 	end.
 
+%% 初始化记录
+tabLoop() ->
+	% ets:new(?tabInfo,[{keypos,#user.name},named_table,ordered_set,public]), %% 有调用的进程都可操作
+	ets:new(?tabInfo,[{keypos,#user.name},named_table,ordered_set]), %% 只能在当前进程操作
+	clientLoop().
 
-
-logout() ->
+%% 处理客户端的操作
+clientLoop() ->
 	receive
-	{Pid, {logout,Response}} ->
-		Response
+		{opt,Socket,Pack} ->
+			case Pack of
+				{register,{Name,Pwd}} ->
+					gen_tcp:send(Socket,term_to_binary(regis(Name,Pwd)));
+				{login,{Name,Pwd}}  ->
+					gen_tcp:send(Socket,term_to_binary(login(Name,Pwd,Socket)));
+				{out,{Name}}  ->
+					Info = out(Socket,Name),
+					gen_tcp:send(Socket,term_to_binary(Info)),
+					case Info of %% 只有自己退出成功,才关闭socket
+						{true,_Prop} ->
+							gen_tcp:close(Socket);
+						{false,_Prop} -> noop
+					end;
+				{mms,{Name,Msg}} ->
+					mmss(Socket,Name,Msg);
+				{mmall,{Msg}} ->
+					mmall(Socket,Msg);
+				{onli} ->
+					gen_tcp:send(Socket,term_to_binary(onli()))
+			end,
+			clientLoop()
 	end.
-%%chat() ->
-%%	.
+
+%% 注册
+regis(Name,Pwd) when is_list(Name) ->
+	Names = string:strip(Name),
+	case ets:lookup(?tabInfo,Names) of
+		[_] ->
+			{false,"already register name"};%% 已经注册的名字
+		[] ->
+			outInfo("regis :"++Name,Pwd),
+			ets:insert(?tabInfo,#user{name=Names,pwd=Pwd,online=false,socket=""}),
+			{true,"register succeed"}
+	end.
+
+%% 登入  一个socket只能登入一号
+login(Name,Pwd,Socket) ->
+	case ets:lookup(?tabInfo,Name) of
+		[User] ->
+			if(Pwd == User#user.pwd andalso User#user.online == false) ->
+				case select('$0',Socket,true) of
+					[] ->
+						outInfo("login :"++Name,Socket),
+						ets:update_element(?tabInfo, Name, [{#user.online, true},{#user.socket, Socket}]),
+						{true,"login succeed"};
+					_Obj ->
+						{false,"have a Socket login"}
+				end;
+				(Pwd =/= User#user.pwd) -> {false,"name / pwd  error"};
+				(User#user.online) -> {false,"name already online"}
+			end;
+		[] -> {false," name null "}
+	end.
+
+%% 登出
+out(Socket,Name) ->
+	case ets:lookup(?tabInfo,Name) of
+		[User] ->
+			if (User#user.online andalso Socket==User#user.socket) ->
+				outInfo("out :"++Name,Socket),
+				ets:update_element(?tabInfo, Name, [{#user.online, false},{#user.socket, 0}]),
+				{true," out succeed"};
+				(User#user.online==false) -> {false,"name out error"};
+				(User#user.socket=/=Socket) -> {false,"name out error not own socket"}
+			end;
+		[] ->
+			{false," name null "}
+	end.
+
+%% 两个聊天
+mmss(Socket,Name,Msg) ->
+	% outInfo(?MODULE,?LINE),
+	case ets:lookup(?tabInfo,Name) of
+		[User] ->
+			if(User#user.online) ->
+				case select('$0',Socket,true) of
+					[] ->
+						noop ;
+					Obj ->
+						outInfo(sel_f(Obj) ++ " to "++Name,Msg),
+						Msgs = sel_f(Obj) ++ " to My : " ++ Msg,
+						MsgMy = " My to " ++ Name ++ " : " ++ Msg,
+						gen_tcp:send(Socket,term_to_binary(MsgMy)),
+						gen_tcp:send(User#user.socket,term_to_binary(Msgs))
+				end;
+				(User#user.online==false) ->
+					gen_tcp:send(Socket,term_to_binary({false," call fail"}))
+			end;
+		[] ->
+			gen_tcp:send(Socket,term_to_binary({false,"name error ,call fail"}))
+	end.
+
+%% 全体聊天
+mmall(Socket,Msg) ->
+	% outInfo(?MODULE,?LINE),
+	case select('$0',Socket,true) of
+		[] ->
+			noop_My ;
+		Obj ->
+			Msgs = sel_f(Obj) ++ " to : " ++ Msg,
+			outInfo(sel_f(Obj) ++ " to ",Msg),
+			case select('_','$1',true) of
+				[] ->
+					noop_all ;
+				Sockets ->
+					send_all_msg(Sockets,Msgs)
+				% gen_tcp:send(User#user.socket,term_to_binary(Msgs));
+			end
+	end.
+
+%% 查询在线玩家
+onli() ->
+	% outInfo(?MODULE,?LINE),
+	case select('$0','_',true) of
+		[] ->
+			{false, onli_null} ;
+		Obj ->
+			Obj
+	end.
+
+%% 广播所有人
+send_all_msg(Sockets, Msg) ->
+	SendData = fun(Socket) ->
+		gen_tcp:send(Socket, term_to_binary(Msg))
+			   end,
+	[SendData(X)||[X] <- Sockets].
+
+%%  查询当前Name,Socket的Online的所有记录
+select(Name,Socket,Online) ->
+	% [User] = ets:tab2list(?tabInfo),
+	ets:match(?tabInfo,{'_',Name,Socket,Online,'_','_'}).
+
+%% 获得一个玩家
+sel_f([]) -> [];
+sel_f([[X]|_]) -> X.
+
+%% 输出错误行
+outInfo(ClassName,Line) ->
+	io:format("__~p______line: ~p~n",[ClassName,Line]).
+
+
+
+
+
+
+%%start() -> register(server, spawn(fun() -> loop() end)).
 %%
+%%store(Key, Value) -> rpc({store, Key, Value}). %%<label id="server.store1"/>
+%%
+%%lookup(Key) -> rpc({lookup, Key}). %%<label id="server.lookup1"/>
+%%
+%%rpc(Q) ->
+%%	server ! {self(), Q},
+%%	receive
+%%		{server, Reply} ->
+%%			io:format("reply: ~p~n",[Reply])
+%%	end.
+%%
+%%loop() ->  %%<label id="server.loop"/>
+%%	receive
+%%		{From, {store, Key, Value}} ->  %%<label id="server.store2"/>
+%%			put(Key, {ok, Value}),
+%%			From ! {server, true},
+%%			loop();
+%%		{From, {lookup, Key}} -> %%<label id="server.lookup2"/>
+%%			From ! {server, get(Key)},
+%%			loop();
+%%
+%%		{From, {name, Name}} ->
+%%			case get(Name) of
+%%			undefined ->
+%%%%				io:format("Server:this Name is accept"),
+%%				From!{server,"Server:this Name is accept~n"} ,
+%%				store(Name,1),
+%%				loop();
+%%			true ->
+%%%%				io:format("Server:this Name is Exist"),
+%%				From ! {self(),"exist"},
+%%				loop()
+%%			end;
+%%		{From,{client1,Message}} ->
+%%			client1!{server,{message,Message}}
+%%	end.
+%%
+%%
+%%chat() ->
+%%	io:format("chat start~n"),
+%%	receive
+%%		}
+%%	end,
+%%	chat().
+
+
+
+
+%%start()   ->
+%%%%	register(server,self()) ,
+%%%%	io:format("regesiter ok ~n"),
+%%	spawn(task4_server,login_loop,[]),
+%%	spawn(task4_server,logout,[]),
+%%	spawn(task4_server,chat,[]),
+%%	spawn(task4_server,message_recv,[])
+%%		 .
+%%
+%%login_loop() ->
+%%	io:format("login start~n"),
+%%	receive
+%%		{_From, {name, Name}} ->
+%%			Name
+%%
+%%
+%%
+%%%%		{From, {name, Name}} ->
+%%%%			case whereis(Name) of
+%%%%			undefined ->
+%%%%				io:format("Server:this Name is accept"),
+%%%%				From!{self(),["Server:this Name is accept" ]} ,
+%%%%				register(Name,From);
+%%%%			_Pid ->
+%%%%				io:format("Server:this Name is Exist"),
+%%%%				From ! {self(),["Server: this name  is Exist "]}
+%%%%			end
+%%	end,
+%%	login_loop().
+%%
+%%
+%%
+%%logout() ->
+%%	io:format("logout start~n"),
+%%	receive
+%%		{_From, {logout,Response}} ->
+%%			io:format("server:~p~n",[Response])
+%%
+%%	end,
+%%	logout().
+%%
+%%
+%%message_recv() ->
+%%	receive
+%%		{_From,{message,Message}} ->
+%%			Message
+%%
+%%	end.
+%%
+%%
+%%chat() ->
+%%	io:format("chat start~n"),
+%%	receive
+%%		{From,{message,Other,Message}} ->
+%%			Other!{self(),{message,From,Message}}
+%%	end,
+%%	chat().
+
 %%
 %%broadcast() _>
 %%	.
