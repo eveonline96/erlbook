8a0e127c0d73f2382c6c5f4f62b2a26fd800c7c1
Eric <870402433@qq.com>
2018-03-16 18:15:17 +0800

    commit doc

---
+++ src/suyoutask/chat_client.erl
@@ -0,0 +1,59 @@
+-module (chat_client).
+-export ([get_servernode/0,logon/1,logoff/0,send/2,client/2]).
+
+%%get_servernode() ->
+%%	chat_server@YMS20180303.
+
+get_servernode() ->
+    chat_server@YMS20180303.
+
+% 用户命令
+logon(Name) ->
+% 首先检查用户自身是否在系统中运行（是否可以查找到 mess_client 进程）
+    case whereis(mess_client) of 
+        undefined ->
+            register(mess_client, 
+                     spawn(chat_client, client, [get_servernode(), Name]));
+        _ -> already_logged_on
+    end.
+
+logoff() ->
+    mess_client ! logoff.
+
+%如果用户存在则将消息发送给 mess_client：
+send(ToName, Message) ->
+    case whereis(mess_client) of 
+        undefined ->
+            not_logged_on;
+        _ -> mess_client ! {message_to, ToName, Message}
+
+end.
+
+% 在每个节点上运行的客户端进程
+client(Server_Node, Name) ->
+    {messenger, Server_Node} ! {self(), logon, Name},%检查是否已登陆
+    await_result(),
+    client(Server_Node).
+
+client(Server_Node) ->
+    receive
+        logoff ->
+            {messenger, Server_Node} ! {self(), logoff},
+            exit(normal);
+        {message_to, ToName, Message} ->
+            {messenger, Server_Node} ! {self(), message_to, ToName, Message},
+            await_result();
+        {message_from, FromName, Message} ->
+            io:format("Message from ~p: ~p~n", [FromName, Message])
+    end,
+    client(Server_Node).
+
+% 等待服务器响应
+await_result() ->
+    receive
+        {messenger, stop, Why} -> % 停止客户端
+            io:format("~p~n", [Why]),
+            exit(normal);
+        {messenger, What} ->  % 正常响应
+            io:format("~p~n", [What])
+    end.
---
+++ src/suyoutask/chat_server.erl
@@ -0,0 +1,59 @@
+-module (chat_server).
+-export ([start_server/0, server/1]).
+
+
+%服务器进程
+%用户列表格式：[{ClientPid1,Name1},{ClientPid2,Name2}...]
+server(User_List) ->
+    receive
+        {From, logon, Name} ->
+            New_User_List = server_logon(From, Name, User_List),
+            server(New_User_List);
+        {From, logoff} ->
+            New_User_List = server_logoff(From, User_List),
+            server(New_User_List);
+        {From, message_to, To, Message} ->
+            server_transfer(From, To, Message, User_List),
+            io:format("list is now: ~p~n", [User_List]),
+            server(User_List)
+    end.
+
+% 启动服务器
+start_server() ->
+    register(messenger, spawn(messenger, server, [[]])).
+
+% 服务器将新用户添加到用户列表
+server_logon(From, Name, User_List) ->
+    % 检查是否在其他地方登录
+    case lists:keymember(Name, 2, User_List) of
+        true ->
+            From ! {messenger, stop, user_exists_at_other_node},  %拒绝登陆
+            User_List;
+        false ->
+            From ! {messenger, logged_on},
+            [{From, Name} | User_List]        %想列表中添加用户
+    end.
+
+% 服务器从用户列表中删除用户
+server_logoff(From, User_List) ->
+    lists:keydelete(From, 1, User_List).
+
+% 服务器在用户之间传输消息
+server_transfer(From, To, Message, User_List) ->
+    % 检查用户是否登录以及他是谁
+    case lists:keysearch(From, 1, User_List) of
+        false ->
+            From ! {messenger, stop, you_are_not_logged_on};
+        {value, {From, Name}} ->
+            server_transfer(From, Name, To, Message, User_List)
+    end.
+% 如果用户存在，则发送消息
+server_transfer(From, Name, To, Message, User_List) ->
+    % 找到接收者并发送消息
+    case lists:keysearch(To, 2, User_List) of
+        false ->
+            From ! {messenger, receiver_not_found};
+        {value, {ToPid, To}} ->
+            ToPid ! {message_from, Name, Message}, 
+            From ! {messenger, sent} 
+    end.
